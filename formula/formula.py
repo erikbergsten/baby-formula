441854238c847e130804c944f2a4a9c128c9fadf
erik <erikbergsten94@gmail.com>
2026-01-04 19:18:17 +0100

    many changes...

--- formula/formula.py
+++ formula/formula.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, Field
+from typing import Annotated
 from jsonpath_ng import parse
 from datetime import datetime
 import io
@@ -7,7 +8,6 @@ from jsonref import replace_refs
 
 
 env = Environment(loader=PackageLoader("formula"), autoescape=select_autoescape())
-
 form_tpl = env.get_template("form.html")
 
 class Item(BaseModel):
@@ -17,21 +17,85 @@ class Item(BaseModel):
 class Metadata(BaseModel):
     category: str
     total: float
-    tags: list[str]
+    tags: list[Annotated[str, Field(min_length=1)]] = []
 
 def always(x):
     return True
 
-class FormModel(BaseModel):
+def path_from_validation_error_location(loc):
+    res = loc[0]
+    for part in loc[1:]:
+        if isinstance(part, str):
+            res += "." + part 
+        elif isinstance(part, int):
+            res += f"[{part}]"
+    return res
+
+class Form:
+    def __init__(self, dataclass, state={}):
+        self.dataclass = dataclass
+        self.state = state
+        self.errors = {}
+        self.value = None
+
+    def from_request_body(dataclass, body):
+        state = {}
+        for key, value in body.multi_items():
+            path = parse(key)
+            path.update_or_create(state, value)
+        return Form(dataclass, state)
+
+    async def from_request(dataclass, request):
+        request_body = await request.form()
+        form = Form.from_request_body(Receipt, request_body)
+
+        if request_body.get("delete"):
+            path = request_body["delete"]
+            form.delete(path)
+            return (form, "delete")
+        elif request_body.get("add"):
+            path = request_body["add"]
+            form.add(path)
+            return (form, "add")
+
+        return form, "submit"
+
+    def validate(self):
+        try:
+            self.value = self.dataclass.model_validate(self.state)
+            return True
+        except ValidationError as validation_error:
+            self.errors = {}
+            for err in validation_error.errors():
+                path = path_from_validation_error_location(err['loc'])
+                self.errors[path] = err['msg']
+            return False
 
     def delete(self, path):
         expr = parse(path)
-        dump = self.model_dump()
-        updated = expr.filter(always, dump)
-        return self.model_validate(updated)
+        expr.filter(always, self.state)
 
-class Receipt(FormModel):
-    store: str
+    def add(self, path):
+        expr = parse(path)
+        values = expr.find(self.state)
+        value = []
+        if len(values) > 0:
+            value = values[0].value
+        value.append("")
+        expr.update_or_create(self.state, value)
+
+    def render(self, href):
+        schema = self.dataclass.model_json_schema()
+        deref_schema = replace_refs(schema)
+        print("rendering value:", self.state)
+        return form_tpl.render(
+                schema=deref_schema,
+                value=self.state,
+                href=href,
+                errors=self.errors)
+
+class Receipt(BaseModel):
+    store: Annotated[str, Field(min_length=1)]
     date: datetime
     metadata: Metadata
     items: list[Item] = []
@@ -42,37 +106,19 @@ def empty(value):
     elif isinstance(value, dict):
         # check all values
         for v in value.values():
-            if value != "":
-                return True
-        return False
+            if not empty(value):
+                return False
+        return True
     elif isinstance(value, list):
         for v in value:
-            if value != "":
-                return True
-        return False
-
-def build_dict_from_form(form):
-    d = {}
-    for key, value in form.multi_items():
-        path = parse(key)
-        if value != "":
-            path.update_or_create(d, value)
-    return d
-
-def validate_form(dataclass, form):
-    d = build_dict_from_form(form)
-    print("dict:", d)
-    return dataclass.model_validate(d)
-
-def generate_form(value, href):
-    schema = value.model_json_schema()
-    deref_schema = replace_refs(schema)
-    return form_tpl.render(schema=deref_schema, value=value, href=href)
+            if not empty(value):
+                return False
+        return True
 
 metadata=Metadata(category="food", total=13.37, tags=["groceries", "expense"])
-
 items=[
     Item(name="foo", price=12.3),
     Item(name="bar", price=32.1),
 ]
 receipt = Receipt(store="ica", date="2025-01-01",  metadata=metadata, items=items)
+f = Form(Receipt, state=receipt.model_dump())
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from typing import Annotated
 from datetime import datetime
 
-from formula import formula
+from formula.formula import Form, Receipt, f
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -16,45 +16,27 @@ templates = Jinja2Templates(directory="templates")
 async def root(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html", context={
-            "form": formula.generate_form(formula.receipt, "/")
+            "form": f.render("/")
         } 
     )
 
-@app.delete("/")
-async def delete_root(request: Request):
-    try:
-        f = await request.form()
-        r = formula.validate_form(formula.Receipt, f)
-        print(r)
-    except Exception as e:
-        print("no form!", e)
-        pass
-    return templates.TemplateResponse(
-        request=request, name="index.html", context={
-            "form": formula.generate_form(formula.receipt, "/")
-        } 
-    )
 @app.post("/")
 async def post_root(request: Request):
-    result = formula.receipt
-
-    try:
-        f = await request.form()
-        r = formula.validate_form(formula.Receipt, f)
-        print(r)
-        if f.get("delete"):
-            delete = f["delete"]
-            print("deleting:", delete)
-            result = r.delete(delete)
+    form, action = await Form.from_request(Receipt, request)
+    if action == "delete":
+        print("user deleted?")
+    elif action == "submit":
+        print("submit attemped!")
+        if form.validate():
+            # success - perform action
+            print("very good!")
         else:
-            print("true submit")
-            result = r
-    except Exception as e:
-        print("no form!", e)
-        pass
+            # failed - render with errors
+            print("very bad!")
+
     return templates.TemplateResponse(
         request=request, name="index.html", context={
-            "form": formula.generate_form(result, "/")
+            "form": form.render("/")
         } 
     )
 
